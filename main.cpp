263923c2ea1bb6f713366c3aac952a8dceeb7f99
Max Base <MaxBaseCode@Gmail.Com>
2021-03-12 20:09:35 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,12 @@
 #include <iostream>
-#include <fstream>
-#include <iostream>
-#include <sstream> //std::stringstream
-
-#include "json.hpp"
-
-using JSon = nlohmann::json;
-
-using if_streamer    = std::ifstream;
-
-struct Globalization {
-  inline static JSon GET = JSon::parse(if_streamer("input.json"));
-};
+#include "my_language.hpp"
 
 int main(int argc, const char * argv[]) {
-    std::ifstream inFile;
-    inFile.open("input.json");
-    std::stringstream strStream;
-    strStream << inFile.rdbuf();
-    std::string str = strStream.str();
-    // std::cout << str << "\n";
 
-    for (auto t : Globalization::GET["exceptions"]) {
-        std::cout << "============\n";
-        std::cout << t["language"]<<",";
-        std::cout << t["code"]<<",";
-        std::cout << t["ltr"]<<"\n";
-        for (auto i : t["translations"]) {
-            std::cout << i["word_key"] << ":" << i["default_value"] << std::endl;
-        }
-    }
+    MyLanguage lang;
+    // TODO: lang.parseFile("input.json");
+    lang.parse();
+    lang.log();
 
     return 0;
 }
