b8914f75c30dd12b3107cd8b62e117e66e87573e
Max Base <MaxBaseCode@Gmail.Com>
2021-03-12 20:09:17 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,19 +14,22 @@ struct Globalization {
 };
 
 int main(int argc, const char * argv[]) {
-  std::ifstream inFile;
-  inFile.open("input.json");
-  std::stringstream strStream;
-  strStream << inFile.rdbuf();
-  std::string str = strStream.str();
-  // std::cout << str << "\n";
+    std::ifstream inFile;
+    inFile.open("input.json");
+    std::stringstream strStream;
+    strStream << inFile.rdbuf();
+    std::string str = strStream.str();
+    // std::cout << str << "\n";
 
-  for (auto t : Globalization::GET["exceptions"]) {
-    for (auto i : t["translations"]) {
-      std::cout << i["word_key"] << ":" << i["default_value"] << std::endl;
-      // std::cout << "l.second: " << l.second << std::endl;
+    for (auto t : Globalization::GET["exceptions"]) {
+        std::cout << "============\n";
+        std::cout << t["language"]<<",";
+        std::cout << t["code"]<<",";
+        std::cout << t["ltr"]<<"\n";
+        for (auto i : t["translations"]) {
+            std::cout << i["word_key"] << ":" << i["default_value"] << std::endl;
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
