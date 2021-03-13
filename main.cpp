a23d62a5bbbdd00219ea6744677c5b33f264ccfd
Max Base <MaxBaseCode@Gmail.Com>
2021-03-13 21:15:49 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,39 +9,39 @@ int main(int argc, const char * argv[]) {
 
     lang.log();
 
-    std::cout << "------------------------------ [ TEST getKey ] -----------------------------\n";
+    // std::cout << "------------------------------ [ TEST getKey ] -----------------------------\n";
 
-    std::cout << lang.getKey("fa_IR", "error").default_value << "\n";
+    // std::cout << lang.getKey("exceptions", "fa_IR", "error").default_value << "\n";
 
-    std::cout << lang.getKey("en_US", "error").default_value << "\n";
+    // std::cout << lang.getKey("exceptions", "en_US", "error").default_value << "\n";
 
-    std::cout << "------------------------------ [ TEST getKey has_error ] -----------------------------\n";
+    // std::cout << "------------------------------ [ TEST getKey has_error ] -----------------------------\n";
 
-    auto i = lang.getKey("en_US", "_bad_keyword_bad_");
-    if(i.has_error) {
-        std::cout << "A error...\n";
-    }
-    else {
-        std::cout << i.has_error << "\n";
-    }
+    // auto i = lang.getKey("exceptions", "en_US", "_bad_keyword_bad_");
+    // if(i.has_error) {
+    //     std::cout << "A error...\n";
+    // }
+    // else {
+    //     std::cout << i.has_error << "\n";
+    // }
 
-    std::cout << "------------------------------ [ TEST hasKey ] -----------------------------\n";
+    // std::cout << "------------------------------ [ TEST hasKey ] -----------------------------\n";
 
-    bool j = lang.hasKey("en_US", "_bad_keyword_bad_");
-    if(j == true) {
-        std::cout << "No error!\n" << "\n";
-    }
-    else {
-        std::cout << "A error...\n";
-    }
+    // bool j = lang.hasKey("exceptions", "en_US", "_bad_keyword_bad_");
+    // if(j == true) {
+    //     std::cout << "No error!\n" << "\n";
+    // }
+    // else {
+    //     std::cout << "A error...\n";
+    // }
 
-    j = lang.hasKey("en_US", "error");
-    if(j == true) {
-        std::cout << "No error!\n" << "\n";
-    }
-    else {
-        std::cout << "A error...\n";
-    }
+    // j = lang.hasKey("exceptions", "en_US", "error");
+    // if(j == true) {
+    //     std::cout << "No error!\n" << "\n";
+    // }
+    // else {
+    //     std::cout << "A error...\n";
+    // }
 
     return 0;
 }
