8f285d91ebe2acdadc5318ad11c8b7b052915f9b
Max Base <MaxBaseCode@Gmail.Com>
2021-03-30 20:05:36 +0430

    Improve to modern c++
    Thanks from @KambizAsadzadeh

--- my_language.cpp
+++ my_language.cpp
@@ -1,202 +1,96 @@
 /*
  * @Name: Cpp-MyLanguage
- * @Date: 12 March 2021, 13 March 2021
+ * @Date: 12 March 2021; 13 March 2021; 28, 29, 30 March 2021
  * @Author: Max Base
  * @Repository: https://github.com/BaseMax/CPP-MyLanguage
  */
 
-#include "my_language.hpp"
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <sstream> //std::stringstream
 
-void MyLanguage::display_word(myword w) {
-    std::cout << "\t\t\t"<<"word_key: " << w.word_key<<"\n";
-    std::cout << "\t\t\t"<<"module: " << w.module<<"\n";
-    std::cout << "\t\t\t"<<"default_value: " << w.default_value<<"\n";
-    std::cout << "\t\t\t"<<"custom_value: " << w.custom_value<<"\n";
-    std::cout << "\t\t\t"<<"status: " << w.status<<"\n";
-}
+#include "json/json.hpp"
 
-std::string MyLanguage::readFile(std::string filename) {
-    std::ifstream inFile;
-    inFile.open("input.json");
-    std::stringstream strStream;
-    strStream << inFile.rdbuf();
-    std::string str = strStream.str();
-    // LOG: std::cout << str << "\n";
-    return str;
-}
+using JSon = nlohmann::json;
 
-void MyLanguage::logLangs(void) {
-    // std::cout << "logLangs\n";
-    std::cout << "---------------- [LOG LANGS] ---------------\n";
-    for (auto s = mymap.begin(); s != mymap.end(); ++s) {
-        std::cout << s->first << ":\n";
-        for (auto itr = s->second.begin(); itr != s->second.end(); ++itr) {
-            std::cout << "\t" << itr->first << "\n";
-        }
-    }
+using if_streamer = std::ifstream;
 
-}
+struct Globalization {
+  inline static JSon GET = JSon::parse(if_streamer("/Users/compez/drogon/build/Tegra/translations/contents2.json"));
+};
 
-void MyLanguage::logWords(void) {
-    // std::cout << "logWords\n";
-    std::cout << "---------------- [LOG WORDS] ---------------\n";
-    for (auto s = mymap.begin(); s != mymap.end(); ++s) {
-        // std::cout << s->first << ":\n";
-        for (auto itr = s->second.begin(); itr != s->second.end(); ++itr) {
-            // std::cout << "\t"<<itr->first << "\n";
-            auto childs = itr->second;
-            for (auto itr = childs.begin(); itr != childs.end(); ++itr) {
-                // std::cout << "\t\t" << itr->first << "\n";
-                display_word(itr->second);
-            }
-        }
-    }
-}
+class LanguageStruct {
+public:
 
-void MyLanguage::log(void) {
-    std::cout << "log\n";
-    logLangs();
-    logWords();
-}
+  static std::shared_ptr<LanguageStruct> getInstance;
+  //static LanguageStruct* getInstance;
 
-void MyLanguage::parseLangs(void) {
-    std::cout << "parseLangs\n";
-    for (auto t : Globalization::GET["languages"]) {
-        std::cout << t << "\n";
-        // {"code":"en_US","language":"english","ltr":true}
-        // {"code":"fa_IR","language":"persian","ltr":false}
-        std::string language = t["language"].get<std::string>();
-        std::string code = t["code"].get<std::string>();
-        bool ltr = t["ltr"];
+  void deleteInstance();
 
-        mylanguages[code] = {
-            language,
-            code,
-            ltr
-        };
-    }
-}
+  LanguageStruct(
+      bool ltr,
+      const std::string& word_key,
+      const std::string& module,
+      const std::string& default_value ,
+      const std::string& custom_value,
+      bool status);
+public:
+  std::string language() const;
+  std::string code() const;
+  bool ltr() const;
+  bool has_error() const;
+  std::string word_key() const;
+  std::string module() const;
+  std::string default_value() const;
+  std::string custom_value() const;
+  bool status() const;
 
-void MyLanguage::parseWords(void) {
-    // std::cout << "parseWords\n";
-    auto items = Globalization::GET.items();
-    for (auto& [key, value] : items) {
-        // key values are: exceptions, global, languages, ...
-        // std::cout << "\n\n----------------------\n";
-        // std::cout << key << " : " << value << "\n";
-        std::map<std::string, std::map<std::string, myword>> mysheet;
-        if(key == "languages") {
-            // solved bug: terminate called after throwing an instance of 'nlohmann::detail::type_error' what():  [json.exception.type_error.305] cannot use operator[] with a string argument with string
-            continue;
-        }
-        for (auto t : Globalization::GET[ key ].items()) {
-            // std::cout << t << "\n";
-            std::map<std::string, myword> v;
-            for (auto i : t.value()) {
-                // std::cout << "\nin loop\n";
-                // std::cout << i << "\n";
-                // std::cout << i["word_key"].get<std::string>() << ":" << i["default_value"].get<std::string>() << std::endl;
-                myword v_myword = {
-                    false,
-                    i["word_key"].get<std::string>(),
-                    i["module"].get<std::string>(),
-                    i["default_value"].get<std::string>(),
-                    i["custom_value"].get<std::string>(),
-                    i["status"],
-                };
-                v.insert(
-                    std::pair<std::string, myword>(
-                        i["word_key"].get<std::string>(),
-                        v_myword
-                    )
-                );
-            }
-            mysheet[t.key()] = v;
-        }
-        mymap[key] = mysheet;
-    }
-}
+private:
 
-/*
- * parse()
- * Arguments: void
- * std::string filename: path or filename to load JSON file. e.g: input.json
- */
-void MyLanguage::parse(void) {
-    // call parseLangs
-    parseLangs();
+  static LanguageStruct* m_instance;
 
-    // call parseWords
-    parseWords();
-}
+  bool m_ltr = false;
+  std::string m_language;
+  std::string  m_code;
 
-/*
- * parseFile(filename)
- * Arguments:
- * std::string filename: path or filename to load JSON file. e.g: input.json
- */
-void MyLanguage::parseFile(std::string filename) {
-    // TODO
-    // struct Globalization {
-    //  inline static JSon GET = JSon::parse(if_streamer("input.json"));
-    // };
+  bool m_has_error;
+  std::string m_word_key;
+  std::string m_module;
+  std::string m_default_value;
+  std::string m_custom_value;
+  bool m_status;
 
-    parse();
-}
+};
 
-/*
- * hasKey(lang, key)
- * Arguments:
- * std::string sheet: name of your selected sheet
- * std::string lang: code of the language structure. e.g: en_US, or fa_IR
- * std::string key: word_key of the `myword` structure. e.g: error, warning
- */
-bool MyLanguage::hasKey(std::string sheet, std::string lang, std::string key) {
-    for (auto s = mymap.begin(); s != mymap.end(); ++s) {
-        if(s->first != sheet) {
-            continue;
-        }
-        for (auto itr = s->second.begin(); itr != s->second.end(); ++itr) {
-            if(itr->first != lang) {
-                continue;
-            }
-            auto childs = itr->second;
-            for (auto itr2 = childs.begin(); itr2 != childs.end(); ++itr2) {
-                if(itr2->first == key) {
-                    return true;
-                }
-            }
-        }
-    }
+class Language {
+public:
+  Language();
+  ~Language();
 
-    return false;
-}
+public:
+  void parse();
+  void parseLangs();
+  void parseWords();
 
-/*
- * getKey(lang, key)
- * Arguments:
- * std::string sheet: name of your selected sheet
- * std::string lang: code of the language structure. e.g: en_US, or fa_IR
- * std::string key: word_key of the `myword` structure. e.g: error, warning
- */
-MyLanguage::myword MyLanguage::getKey(std::string sheet, std::string lang, std::string key) {
-    for (auto s = mymap.begin(); s != mymap.end(); ++s) {
-        if(s->first != sheet) {
-            continue;
-        }
-        for (auto itr = s->second.begin(); itr != s->second.end(); ++itr) {
-            if(itr->first != lang) {
-                continue;
-            }
-            auto childs = itr->second;
-            for (auto itr2 = childs.begin(); itr2 != childs.end(); ++itr2) {
-                if(itr2->first == key) {
-                    return itr2->second;
-                }
-            }
-        }
-    }
+  void parseFile(const std::string&  filename);
+  void displayWord(LanguageStruct w);
 
-    // TODO: use pointer and: return NULL;
-    return {true};
-}
+  void log();
+  void logLangs();
+  void logWords();
+
+
+  bool hasKey(const std::string& sheet, const std::string& lang, const std::string& key);
+
+  LanguageStruct getString(const std::string& sheet, const std::string& lang, const std::string& key);
+
+  std::string readFile(const std::string&  filename);
+
+private:
+  std::map<std::string, LanguageStruct> m_languages;
+  std::map<std::string, std::map<std::string, std::map<std::string, LanguageStruct>>> m_map;
+  //std::shared_ptr<std::map<std::string, std::map<std::string, std::map<std::string, LanguageStruct>>>> m_map;
+
+};
+
